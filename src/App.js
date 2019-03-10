b4872e913015788bdfc9db5100c55727bb350b93
Michał Sipa <michal.sipa@mirumee.com>
2019-03-10 16:37:53 +0100

    Add deletePersonHandler

--- src/App.js
+++ src/App.js
@@ -12,17 +12,11 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = newName => {
-    // console.log("Clicked");
-    // DON'T DO THIS this.state.persons[0].name = "Maximilian";
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 27 }
-      ]
-    });
-  };
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
 
   nameChangedHandler = event => {
     this.setState({
@@ -53,8 +47,11 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age}/>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              click={() => this.deletePersonHandler(index)} 
+              name={person.name} 
+              age={person.age}/>
           })}
         </div>
       );
